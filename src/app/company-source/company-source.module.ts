cfbdfb26ff6de6b06f2e4ed6faa34797b93e808b
Hader <hader.araujo@gmail.com>
2018-02-21 19:20:06 -0300

    Flexbox added

--- src/app/company-source/company-source.module.ts
+++ src/app/company-source/company-source.module.ts
@@ -2,17 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MaterialModule } from '../shared/material.module';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { CompanySourceRoutingModule } from './company-source-routing.module';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
+    FlexLayoutModule,
     FormsModule,
     CompanySourceRoutingModule
   ],
-  declarations: [ButtonsComponent]
+  declarations: [ButtonsComponent, FlexboxComponent]
 })
 export class CompanySourceModule { }
