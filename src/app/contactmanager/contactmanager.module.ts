73c0d42aadb965825ed12b18bbbaf367a8ff595e
Hader <hader.araujo@gmail.com>
2018-02-24 22:46:55 -0300

    Main content card added

--- src/app/contactmanager/contactmanager.module.ts
+++ src/app/contactmanager/contactmanager.module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
   {
     path: '', component: ContactManagerAppComponent,
     children: [
+      { path: ':id', component: MainContentComponent },
       { path: '', component: MainContentComponent }
     ]
   },
